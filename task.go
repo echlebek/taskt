4467e188b4c71f957d0b35a0d7de43e1621b7558
echlebek <echlebek@gmail.com>
2017-09-29 17:49:23 -0700

    Add TaskRequest and TaskResult.
    TaskRequest and TaskResult are the basic data structures that are sent
    over the wire as JSON.

---
+++ task.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type MSDuration time.Duration
+
+func (m *MSDuration) MarshalJSON() ([]byte, error) {
+	ms := float64(*m) / float64(time.Millisecond)
+	return json.Marshal(ms)
+}
+
+func (m *MSDuration) UnmarshalJSON(b []byte) error {
+	var f float64
+	if err := json.Unmarshal(b, &f); err != nil {
+		return fmt.Errorf("couldn't unmarshal MSDuration: %s", err)
+	}
+	*m = MSDuration(f * float64(time.Millisecond))
+	return nil
+}
+
+// TaskRequest is a request to run a task.
+type TaskRequest struct {
+	Command []string `json:"command"`
+	Timeout int64    `json:"timeout"`
+}
+
+// TaskResult is the result of running a task.
+type TaskResult struct {
+	Command    []string   `json:"command"`
+	ExecutedAt int64      `json:"executed_at"`
+	DurationMS MSDuration `json:"duration_ms"`
+	ExitCode   int        `json:"exit_code"`
+	Output     string     `json:"output"`
+	Error      string     `json:"error"`
+}
